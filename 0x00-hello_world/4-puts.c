2ac7758c9fe90fda42036ca83e94f9c1a8e08e57
wilsonnyabando <wilsonnyabando73@gmail.com>
2023-07-13 21:49:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always o (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (o);
+}
