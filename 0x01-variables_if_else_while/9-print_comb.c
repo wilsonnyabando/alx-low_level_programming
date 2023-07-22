dae362cdbbb9e7eaecdbd48b4af2344d21b8d2a8
wilsonnyabando <wilsonnyabando73@gmail.com>
2023-07-22 07:43:05 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}}
+	putchar('\n');
+	return (0);
+}
